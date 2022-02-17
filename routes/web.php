8fb13b2dec24425252e98f1672013d99a8c4ce12
jeferson-p <jeferson.p@univille.br>
2022-02-17 18:22:53 -0300

    TelaLogin

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +17,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('site.home');
 });
+
+Route::post('login', [AuthController::class, 'login']);
+
+Route::get('dashboard', [DashboardController::class, 'index']);
